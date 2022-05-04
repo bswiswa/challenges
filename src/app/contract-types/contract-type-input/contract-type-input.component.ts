7fce83774c77ef1b0b6328233e29f344d5df9ba2
bswiswa <bswiswa@gmail.com>
2022-05-04 16:22:15 -0400

    Challenge 4 - Forms

--- src/app/contract-types/contract-type-input/contract-type-input.component.ts
+++ src/app/contract-types/contract-type-input/contract-type-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ContractType } from '../shared/contract-type.model';
 
 @Component({
@@ -9,12 +10,23 @@ import { ContractType } from '../shared/contract-type.model';
 export class ContractTypeInputComponent implements OnInit {
   @Input() contractType!: ContractType;
   @Output() cancel = new EventEmitter<void>();
+  contractForm!: FormGroup;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contractForm = new FormGroup({
+      name: new FormControl(this.contractType.name, [Validators.required]),
+    });
+  }
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  log() {
+    if (this.contractForm.get('name')?.valid) {
+      console.log(this.contractForm.value.name);
+    }
   }
 }
--- src/app/contract-types/contract-types.module.ts
+++ src/app/contract-types/contract-types.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ContractTypesListComponent } from './contract-types-list/contract-types-list.component';
 import { ContractTypeInputComponent } from './contract-type-input/contract-type-input.component';
 import { ContractTypeItemComponent } from './contract-type-item/contract-type-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ContractTypesListComponent, ContractTypeInputComponent, ContractTypeItemComponent],
-  imports: [CommonModule],
+  declarations: [
+    ContractTypesListComponent,
+    ContractTypeInputComponent,
+    ContractTypeItemComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [ContractTypesListComponent],
 })
 export class ContractTypesModule {}
